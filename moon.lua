78c6a77ebe0ff258c80f57260db4f9e6ffeb17a1
moonsource <65741848+moonsource@users.noreply.github.com>
2020-05-29 18:15:34 +0300

    Add files via upload

--- moon.lua
+++ moon.lua
@@ -4220,7 +4220,7 @@ x = x + 1
 if database:get(bot_id.."moonsource:User:Name"..v.user_id_) then
 t = t..""..x.." ⌯ [@"..database:get(bot_id.."moonsource:User:Name"..v.user_id_).."]\n"
 else
-t = t..""..x.." - {"..v.user_id_.."}\n"
+t = t..""..x.." [NoUser](https://t.me/SourceMoon) \n"
 end
 end
 send(msg.chat_id_,msg.id_,t)
